8f980398beb28ca005e64d8a93fc4bee8bedc832
Vishnuvarun077 <thorthi.varun@students.iiit.ac.in>
2024-12-20 00:56:01 +0530

    implement few bug fixes

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -5,5 +6,4 @@ export const dynamic = "force-dynamic";
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
   throw new Error("Sentry Example API Route Error");
-  return NextResponse.json({ data: "Testing Sentry Error..." });
 }
